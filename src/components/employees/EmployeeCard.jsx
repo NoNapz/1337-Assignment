4e74cf634adc2a574c77a94fd4d3fe97bb24c3c3
Robin Hellström <robin1robin@hotmail.com>
2022-05-03 19:22:19 +0200

    EmployeeCard Styling + responsiveness

--- src/components/employees/EmployeeCard.jsx
+++ src/components/employees/EmployeeCard.jsx
@@ -11,13 +11,13 @@ function EmployeeCard(props) {
   return (
     <Card>
         <div className={classes.card}>
-            <img src={props.image} alt="Avatar of Employee" />
+            <img src={props.image} alt="Missing Avatar" />
             <div className={classes.body}>
-                <div>
+                <div className={classes.details}>
                   <h2>{props.name}</h2>
                   <span>{props.office}</span>
                 </div>
-                <div className={classes.socials}>
+                <div className={!props.github && !props.twitter && !props.linkedIn ? '' :  (classes.socials)}>
                     {props.github && <button onClick={() => window.open(`https://github.com/${props.github}`, "_blank")}> <AiOutlineGithub /> </button>}
                     {props.twitter && <button onClick={() => window.open(`https://twitter.com/${props.twitter}`, "_blank")}> <AiOutlineTwitter /> </button>}
                     {props.linkedIn && <button onClick={() => window.open(`https://www.linkedin.com${props.linkedIn}`, "_blank")}> <AiOutlineLinkedin /> </button>}
